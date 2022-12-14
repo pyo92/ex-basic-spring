6c18328741702add38142c2552335295e3a3844a
PARK GI-PYO <gipyopark@gmail.com>
2022-12-14 18:53:50 +0900

    #1 - 프로젝트 생성
    - Spring Initializr 를 이용한 프로젝트 생성
    - Dependencies 추가 X

---
+++ src/main/java/com/example/exbasicspring/ExBasicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.exbasicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBasicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExBasicSpringApplication.class, args);
+    }
+
+}
