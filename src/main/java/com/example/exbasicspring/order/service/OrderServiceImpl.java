082a93c3eb95bb0eb82ef33fe905df8104bbe5ec
PARK GI-PYO <gipyopark@gmail.com>
2022-12-28 01:11:09 +0900

    #24 - 구현체 의존관계 자동주입 적용
    - '@Component', '@Autowired' 어노테이션 적용

--- src/main/java/com/example/exbasicspring/order/service/OrderServiceImpl.java
+++ src/main/java/com/example/exbasicspring/order/service/OrderServiceImpl.java
@@ -6,13 +6,17 @@ import com.example.exbasicspring.member.domain.Member;
 import com.example.exbasicspring.member.repository.MemberRepository;
 import com.example.exbasicspring.member.repository.MemoryMemberRepository;
 import com.example.exbasicspring.order.domain.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
 
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     //생성자 주입 방식으로 수정 -> OCP, DIP 만족
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
